4eb603f2081ee21966cd7f55644920a6c322188b
FINDOSKDI <FINDOSKDI@FINDOSKDI.noreply.github.com>
2018-05-13 18:36:41 +0200

    version presentada pr1

--- practica1.py
+++ practica1.py
@@ -2,26 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button, Slider
 from matplotlib.patches import Circle
-import math as math
-
-def xtild(x):
-    return np.concatenate(([[1]*x.shape[1]],x),axis=0)
+import math
 
 colorcodes = {1 : 'r', 2 : 'g', 3 : 'b', 4 : 'c', 5 : 'y'}
 codecolors = {'r' : 1, 'g' : 2, 'b' : 3, 'c' : 4, 'y' : 5}
 codesigns = {1 : 1, 2 : -1}
 signcodes = {1 : 1, -1 : 2}
+
 def codeonehot(k, K):
     return np.array([0]*(k-1)+[1]+[0]*(K-k))
 
-# This won't be necessary
-#def onehotcode(arr):
-#    if float(arr[0]) == 1.0
+#### Algorithms
 
 # Calculates linear form to classify data using the least squares method
 def least_squares(X,t):
-    xtilde = xtild(X)
-    Wtilde = np.dot(np.linalg.inv(np.dot(xtilde,xtilde.T)),np.dot(xtilde,t.T)) # we still need to check the determinant
+    xtilde = np.concatenate(([[1]*X.shape[1]],X),axis=0)
+    Wtilde = np.dot(np.linalg.inv(np.dot(xtilde,xtilde.T)),np.dot(xtilde,t.T))
     return Wtilde
 
 # Calculates linear form to classify data using the Fisher (linear discriminant) method
@@ -54,10 +50,16 @@ def lda(X,t):
     for i in (0, 1):
         s.append(np.dot(np.dot(W.T,var_class_mat[i]),W))
         m.append(np.dot(W.T,mean_vectors[i]))
-    F=[(s[0]-s[1]),(2*(s[1]*m[0])),(-s[1]*pow(m[0],2)+s[0]*pow(m[1],2)-2*s[0]*s[1]*math.log(p[1]/math.sqrt(s[1]))+2*s[0]*s[1]*math.log(p[0]/math.sqrt(s[0])))]
-    c=np.roots([F[0],F[1],F[2]])
-    F_prima=[2*F[0], F[1]]
-    if (math.copysign(c[0]*F_prima[0]-F_prima[1],1))<=(math.copysign(c[1]*F_prima[0]-F_prima[1],1)):
+    F = [(s[0]-s[1]),
+         (2*(s[1]*m[0])),
+         (-s[1]*pow(m[0],2) +
+           s[0]*pow(m[1],2)-2*s[0] *
+           s[1]*math.log(p[1]/math.sqrt(s[1])) +
+           2*s[0]*s[1]*math.log(p[0]/math.sqrt(s[0])))
+        ]
+    c = np.roots([F[0],F[1],F[2]])
+    F_prima = [2*F[0], F[1]]
+    if math.fabs(c[0]*F_prima[0]-F_prima[1]) <= math.fabs(c[1]*F_prima[0]-F_prima[1]):
         wtilde=np.concatenate(([-(c[0]*pow(W[0],2)+c[0]*pow(W[1],2))], W.T)).T
     else:
         wtilde=np.concatenate(([-(c[1]*pow(W[0],2)+c[1]*pow(W[1],2))], W.T)).T
@@ -75,10 +77,7 @@ def classify(x,w):
     else:
         return np.argmax(w.T.dot(xtilde)) + 1
 
-
-#### GUI
-
-
+#### Drawing class
 class CreatePoints(object):
 
     def __init__(self, fig, ax):
@@ -189,13 +188,7 @@ class CreatePoints(object):
         self.current_circle.center = self.x0 + dx, self.y0 + dy
         self.fig.canvas.draw()
 
-
-fig, ax = plt.subplots()
-ax.set_xlim([-20,20])
-ax.set_ylim([-20,20])
-plt.subplots_adjust(bottom=0.2)
-
-circu = CreatePoints(fig,ax)
+#### Event handlers
 
 def fsquares(event):
     x = circu.getX()
@@ -227,18 +220,6 @@ def ffisher(event):
         ax.plot(a,b,'.{}'.format(colorcodes[clase]))
     fig.canvas.draw()
     
-
-axminsquares = plt.axes([0.7, 0.05, 0.1, 0.075])
-axfisher = plt.axes([0.81, 0.05, 0.1, 0.075])
-bminsquares = Button(axminsquares, 'Squares')
-bminsquares.on_clicked(fsquares)
-bfisher = Button(axfisher, 'Fisher')
-bfisher.on_clicked(ffisher)
-
-axslid = plt.axes([0.5, 0.05, 0.1, 0.075])
-slid = Slider(axslid, 'Classes', 2, 5, valfmt='%d')
-
-K = 2
 def update(val):
     global k
     global K
@@ -266,13 +247,6 @@ def update(val):
         axfisher.axis('on')
         fig.canvas.draw()
 
-slid.on_changed(update)
-
-
-axclass = plt.axes([0.3, 0.05, 0.1, 0.075])
-bclass = Button(axclass, 'Class 1')
-
-k = 1
 def changeclass(event):
     global k
     global bclass
@@ -282,13 +256,41 @@ def changeclass(event):
         k = k + 1
     circu.updateColor(k)
     bclass.label.set_text('Class {}'.format(k))
+
+def fclear(event):
+    circu.clear()
+
+
+#### Setup
+
+K = 2
+k = 1
+
+fig, ax = plt.subplots()
+fig.canvas.set_window_title('Practica 1')
+ax.set_xlim([-20,20])
+ax.set_ylim([-20,20])
+plt.subplots_adjust(bottom=0.2)
+circu = CreatePoints(fig,ax)
+
+axminsquares = plt.axes([0.7, 0.05, 0.1, 0.075])
+bminsquares = Button(axminsquares, 'Squares')
+bminsquares.on_clicked(fsquares)
+
+axfisher = plt.axes([0.81, 0.05, 0.1, 0.075])
+bfisher = Button(axfisher, 'Fisher')
+bfisher.on_clicked(ffisher)
+
+axslid = plt.axes([0.5, 0.05, 0.1, 0.075])
+slid = Slider(axslid, 'Classes', 2, 5, valfmt='%d', valinit=2)
+slid.on_changed(update)
+
+axclass = plt.axes([0.3, 0.05, 0.1, 0.075])
+bclass = Button(axclass, 'Class 1')
 bclass.on_clicked(changeclass)
 
 axclear = plt.axes([0.1, 0.05, 0.1, 0.075])
 bclear = Button(axclear, 'Clear')
-
-def fclear(event):
-    circu.clear()
 bclear.on_clicked(fclear)
 
 plt.show()
